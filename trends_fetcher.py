327a365dd1f659c4cb81c941a467b44ca70680f3
developer-trends <devteam@300mediahub.com>
2025-05-16 11:23:42 +0800

    Update trends_fetcher.py

--- trends_fetcher.py
+++ trends_fetcher.py
@@ -177,7 +177,7 @@ def scrape_all_pages():
 
 
 def trigger_google_apps_script():
-    url = "https://script.google.com/macros/s/AKfycbwRPjRjb_nD_D3gVEuFcClD6nIMrEA75ZQ6bbaFAm0qKuwbzubtQ7sVf7Wq-Z-QicaHBA/exec"
+    url = "https://script.google.com/macros/s/AKfycbwEdWiktuqtXnATjYpfXfgpnFgAT8GU89htD2uO22IusWnVnoMgU6C4AahJprSAZ4-VaQ/exec"
     try:
         response = requests.post(url, timeout=180)
         if response.status_code == 200:
